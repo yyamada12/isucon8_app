4e68d212b318761bd6d57b86e5ef93fe0fca4d01
root <root@localhost.localdomain>
2019-08-16 03:47:00 +0000

    [add] pprof

--- webapp/go/src/torb/app.go
+++ webapp/go/src/torb/app.go
@@ -15,6 +15,8 @@ import (
 	"strconv"
 	"strings"
 	"time"
+	"net/http"
+	_ "net/http/pprof"
 
 	_ "github.com/go-sql-driver/mysql"
 	"github.com/gorilla/sessions"
@@ -310,6 +312,10 @@ func (r *Renderer) Render(w io.Writer, name string, data interface{}, c echo.Con
 var db *sql.DB
 
 func main() {
+	go func() {
+		log.Println(http.ListenAndServe("localhost:6060", nil))
+	}()
+
 	dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?parseTime=true&charset=utf8mb4",
 		os.Getenv("DB_USER"), os.Getenv("DB_PASS"),
 		os.Getenv("DB_HOST"), os.Getenv("DB_PORT"),
@@ -912,6 +918,7 @@ func main() {
 		}
 		return renderReportCSV(c, reports)
 	}, adminLoginRequired)
+
 
 	e.Start(":8080")
 }
